4e09b719c6617222254e8c24b9b7635b8ce7cdb3
Long Nguyen <hlong24399@gmail.com>
2021-10-29 18:23:00 -0700

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YouLook_Official.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
